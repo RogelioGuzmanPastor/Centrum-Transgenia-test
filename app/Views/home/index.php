324dbcdde25dee9d7d6058861c761cb361accc06
Rogelio Guzman Pastor <rogelio.guzman@meridasoft.com>
2023-01-03 23:08:54 -0600

    Implementando plantilla

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -1,5 +1,36 @@
-<main style="min-height: 80vh;" id="home-trasgenia">
+<main id="home-trasgenia">
     <section class="header">
-        <h1></h1>
+        <h1>Get a project quote</h1>
+        <h2>Please fill the form below to receive a quote for your project. Feel free to add as much detail as needed.</h2>
+    </section>
+    <section class="contenedor-pasos">
+        <div class="progreso">
+            <div class="numero active">
+                <span>1</span>
+            </div>
+            <div class="barra">                
+                <div class="relleno" style="width: 50%;"></div>
+            </div>
+            <div class="numero">
+                <span>2</span>
+            </div>
+            <div class="barra">                
+                <div class="relleno"></div>
+            </div>
+            <div class="numero">
+                <span>3</span>
+            </div>
+            <div class="barra">                
+                <div class="relleno"></div>
+            </div>
+            <div class="numero">
+                <span>4</span>
+            </div>
+        </div>
+
+    </section>
+    <section class="contenedor-botones">
+        <a class="btn btn-brix-outline-lg">Previous step</a>
+        <a class="btn btn-brix-lg">Next step</a>
     </section>
 </main>
